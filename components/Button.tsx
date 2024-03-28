49c215abf6694f8db9840075261f96e71c45c1e3
Akin Akinbobola <akinbobola71@gmail.com>
2024-03-28 15:08:53 +0100

    Finish Footer.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,9 +5,10 @@ type ButtonProps = {
     title: string;
     icon?: string;
     variant: string;
+    full?: boolean;
 }
 
-const Button = ({type, title, icon, variant}: ButtonProps) => {
+const Button = ({type, title, icon, variant, full}: ButtonProps) => {
     return (
         <button type={type} className={`${variant} flexCenter rounded-full border gap-3`}>
             {icon && <Image src={icon} alt={title} width={24} height={24}/>}
